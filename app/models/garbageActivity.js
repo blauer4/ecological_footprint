c647806b4963b1f0eccb0813f79eb06bbe3b07f1
Lollixzc <laurence.bonat@studenti.unitn.it>
2022-05-13 16:29:15 +0200

    Added model for Garbage Activity

---
+++ app/models/garbageActivity.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+const materialSchema = require('./material.js').materialSchema;
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('GarbageActivity', new Schema({ 
+    userId: { type: String, required: true },
+    impact: {type: Number, required: true},
+	material: materialSchema,
+	amount: { type: Number, required: true }
+}));
