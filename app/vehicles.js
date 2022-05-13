1d33cc0eebd85a3c1fab7b34c6d22a91e0b7c02e
TudiFrudii <vittoria.ossanna@studenti.unitn.it>
2022-05-13 16:33:47 +0200

    vehicles REST update

--- app/vehicles.js
+++ app/vehicles.js
@@ -2,6 +2,13 @@
  * Gestione listaggio veicoli presenti nel db 
  */
 
- const express = require('express');
- const router = express.Router();
- const Vehicle = require('./models/vehicle.js').Vehicle; 
+const express = require('express');
+const router = express.Router();
+const Vehicle = require('./models/vehicle.js').Vehicle;
+
+router.get('', async (req, res) => {
+
+    let vehicles = await Vehicle.find({});
+
+    res.status(200).json(vehicles);
+});
