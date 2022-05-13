8a6c366ee94a95b21252c33d54fea2143fe1f7fe
TudiFrudii <vittoria.ossanna@studenti.unitn.it>
2022-05-13 16:23:15 +0200

    vehicle model

--- app/models/vehicle.js
+++ app/models/vehicle.js
@@ -4,4 +4,12 @@ var Schema = mongoose.Schema;
 var vehicleSchema = new Schema({ 
     name: { type: String, required: true },
     unitImpact: { type: String, required: true }
-})
+});
+
+var Vehicle = mongoose.model('Vehicle', vehicleSchema);
+
+// set up a mongoose model
+module.exports = {
+    Vehicle,
+    vehicleSchema
+};
