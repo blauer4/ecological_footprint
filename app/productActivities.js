369833456d4549c48dcfa8ff1a33395bfa4442c7
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-13 15:59:56 +0200

    post method for product activities

--- app/productActivities.js
+++ app/productActivities.js
@@ -17,5 +17,31 @@ router.get('/:id', async (req, res) => {
     res.status(200).json(activity);
 });
 
+router.post('', async (req, res) => {
+
+    let userId = req.body["userId"];
+    let productId = req.body["productId"];
+    let amount = req.body["amount"];
+
+    let product = await Product.findById(productId);
+    
+    //TODO calculation of a random impact 
+    let impact = product.unitImpact * amount;
+
+
+    var newActivity = new ProductActivity({
+        userId: userId,
+        amount: amount,
+        impact: impact,
+        product: product
+    });
+    
+    activity = await newActivity.save();
+
+    /**
+     * Return the link to the newly created resource 
+     */
+    res.location("/api/v1/activities/products/" + activity.id).status(201).send();
+});
 
 module.exports = router;
