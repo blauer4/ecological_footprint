6ba0c1d9cd345ab941387cafaae753849a850c08
Lollixzc <laurence.bonat@studenti.unitn.it>
2022-05-14 12:04:51 +0200

    Added garbage to GET api, and changed method

--- app/listActivities.js
+++ app/listActivities.js
@@ -7,11 +7,22 @@ const router = express.Router();
 router.get('',async (req,res)=>{
     let garbage = await garbageActivity.find({});
     let product = await productActivity.find({});
-
+    // TODO: Select only authenticated
     let resp = [];
-    for(item in product){
-        resp.push(product[item]);
+    for(item of product){
+        resp.push({
+            ref: "/api/v1/activities/products/" + item.id,
+            type: "product",
+        });
     }
+
+    for(item of garbage){
+        resp.push({
+            ref: "/api/v1/activities/materials/" + item.id,
+            type: "materials",
+        });
+    }
+    
     res.status(200).json(resp);
 });
 
