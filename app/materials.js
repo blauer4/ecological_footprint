c484ff54a81ad5b7ffcff9e15a7b7f18cc3d65f0
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-14 12:03:38 +0200

    relationships using links - materials

--- app/materials.js
+++ app/materials.js
@@ -8,13 +8,23 @@
  router.get('', async (req, res) => {
  
     let materials = await Material.find({});
-
+    materials = materials.map( (material) => {
+        return {
+            self: '/api/v1/materials/' + material["_id"],
+            name: material["name"]
+        };
+    });
     res.status(200).json(materials);
 });
 
 router.get('/:id', async (req, res) => {
 
     let material = await Material.findById(req.params.id);
+    material = {
+        self: '/api/v1/materials/' + material.id,
+        name: material["name"],
+        unitImpact: material["unitImpact"]
+    }
 
     res.status(200).json(material);
 });
