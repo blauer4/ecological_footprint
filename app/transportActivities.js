aba0cafc45b3bd4278af02748d3a5c94fefba566
TudiFrudii <vittoria.ossanna@studenti.unitn.it>
2022-05-14 11:05:29 +0200

    transportActivities REST

---
+++ app/transportActivities.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const TransportActivity = require('./models/transportActivity.js');
+const Vehicle = require('./models/vehicle.js').Vehicle;
+const router = express.Router();
+
+module.exports = router;
