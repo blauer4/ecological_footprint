6b2fdabfd39318416c99ccabd8788482d066f9ef
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-14 10:40:47 +0200

    remove garbage activity

--- app/garbageActivities.js
+++ app/garbageActivities.js
@@ -48,5 +48,19 @@ router.post('', async (req, res) => {
     res.location("/api/v1/activities/garbage/" + activity.id).status(201).send();
 });
 
+router.delete('/:id', async (req, res) => {
+    let id = req.params["id"];
+
+    let result = await GarbageActivity.deleteOne({_id: id});
+    if (result.deletedCount == 1){
+        console.log(`Documento con id ${id} eliminato con successo`);
+        res.send("OK");
+    }else{
+        console.error(`ERRORE: eliminazione documento con attivita'(garbage) con id ${id}`);
+        res.send("Fail");
+    }
+    
+});
+
 module.exports = router;
 
