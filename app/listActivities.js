fb2d695c20d5391aa0cf25471ace79d4e94da313
Laurence Bonat <laurence.bonat@studenti.unitn.it>
2022-05-14 16:08:55 +0200

    Added transport to listActivities

--- app/listActivities.js
+++ app/listActivities.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const garbageActivity = require('./models/garbageActivity.js');
 const productActivity = require('./models/productActivity.js');
+const transportActivity = require('./models/transportActivity.js');
 
 const router = express.Router();
 
 router.get('',async (req,res)=>{
     let garbage = await garbageActivity.find({});
     let product = await productActivity.find({});
+    let transport = await transportActivity.find({});
     // TODO: Select only authenticated
     let resp = [];
     for(item of product){
@@ -20,6 +22,13 @@ router.get('',async (req,res)=>{
         resp.push({
             self: "/api/v1/activities/materials/" + item.id,
             type: "materials",
+        });
+    }
+
+    for(item of transport){
+        resp.push({
+            self: "/api/v1/activities/transport/" + item.id,
+            type: "transport",
         });
     }
 
