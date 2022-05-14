b4b45a117d847cc2c99dd358160697df2b5bc0c5
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-14 11:57:25 +0200

    relationships using links - vehicles

--- app/vehicles.js
+++ app/vehicles.js
@@ -9,13 +9,23 @@ const Vehicle = require('./models/vehicle.js').Vehicle;
 router.get('', async (req, res) => {
 
     let vehicles = await Vehicle.find({});
-
+    vehicles = vehicles.map( (vehicle) => {
+        return {
+            self: '/api/v1/vehicles/' + vehicle["_id"],
+            name: vehicle["name"]
+        };
+    });
     res.status(200).json(vehicles);
 });
 
 router.get('/:id', async (req, res) => {
 
     let vehicle = await Vehicle.findById(req.params.id);
+    vehicle = {
+        self: '/api/v1/vehicles/' + vehicle.id,
+        name: vehicle["name"],
+        unitImpact: vehicle["unitImpact"]
+    }
 
     res.status(200).json(vehicle);
 });
