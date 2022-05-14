eb5409acdad04f66e6310c28928c469762c9c8af
TudiFrudii <vittoria.ossanna@studenti.unitn.it>
2022-05-14 11:06:17 +0200

    transportActivities GET

--- app/transportActivities.js
+++ app/transportActivities.js
@@ -3,4 +3,19 @@ const TransportActivity = require('./models/transportActivity.js');
 const Vehicle = require('./models/vehicle.js').Vehicle;
 const router = express.Router();
 
+
+router.get('', async (req, res) => {
+
+    let activities = await TransportActivity.find({});
+
+    res.status(200).json(activities);
+});
+
+router.get('/:id', async (req, res) => {
+
+    let activity = await TransportActivity.findById(req.params.id);
+
+    res.status(200).json(activity);
+});
+
 module.exports = router;
