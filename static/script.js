70da6d3e83f95ffc42770f0112e801b70b58cd16
Laurence Bonat <laurence.bonat@studenti.unitn.it>
2022-05-15 12:06:09 +0200

    Load materials on page load

--- static/script.js
+++ static/script.js
@@ -3,31 +3,40 @@
  * dell'applicazione
  */
 
-function loadMaterials(){
+function loadMaterials() {
     fetch('/api/v1/materials')
-    .then((resp)=> resp.json())
-    .then(function(data){
-        console.log(data);
-    });
+        .then((resp) => resp.json())
+        .then(function (data) {
+            let select = document.getElementById("materialSelect");
+            data.forEach(element => {
+                let option = document.createElement("option");
+                option.setAttribute("value", element.self);
+                let text = document.createTextNode(element.name);
+                option.appendChild(text);
+                select.appendChild(option);
+            });
+        }).catch(error => console.error(error));
 }
+
+loadMaterials();
 
 function getProductsByName() {
 
     let item_name = document.getElementById("input_food_item").value;
-    
-    fetch('/api/v1/products')
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
-        
-        data.forEach(element => {
-            if (element["name"].includes(item_name)){
-                console.log(element["name"])
-            }
-        });
 
-    })
-    .catch( error => console.error(error) );// If there is any error you will catch them here
-    
+    fetch('/api/v1/products')
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) { // Here you get the data to modify as you please
+
+            data.forEach(element => {
+                if (element["name"].includes(item_name)) {
+                    console.log(element["name"])
+                }
+            });
+
+        })
+        .catch(error => console.error(error));// If there is any error you will catch them here
+
 }
 
 getProductsByName();
