0ec76d9893c5a11be23531d1e310d01c7bf69f60
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-15 12:15:18 +0200

    list available products and fill the form fields

--- static/script.js
+++ static/script.js
@@ -18,25 +18,61 @@ function loadMaterials() {
         }).catch(error => console.error(error));
 }
 
-loadMaterials();
-
+/**
+ * function that searches for a product by its name and returns the 
+ * list of corresponding products as a select menu
+ */
 function getProductsByName() {
 
     let item_name = document.getElementById("input_food_item").value;
-
+    
     fetch('/api/v1/products')
-        .then((resp) => resp.json()) // Transform the data into json
-        .then(function (data) { // Here you get the data to modify as you please
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { 
+        
+        data = data.filter((elem)=>{
+            return elem["name"].toLowerCase().includes(item_name.toLowerCase());
+        });
 
+        if (data.length <= 4){
+            let res = "";
             data.forEach(element => {
-                if (element["name"].includes(item_name)) {
-                    console.log(element["name"])
-                }
+                res += `<option value="${element['self']}">${element["name"]}</option>`;
             });
 
-        })
-        .catch(error => console.error(error));// If there is any error you will catch them here
+            floatingSelectProduct.innerHTML = res;
+        }
 
+    })
+    .catch( error => console.error(error) ); // error handle
+    
 }
 
+/**
+ * function that fills the add food consumption form fields with the 
+ * select option results of the search of the function getProductsByName
+ */
+function fillProductActivityForm(){
+    let inputFoodName = document.getElementById("input_food_name");
+    let inputFoodCode = document.getElementById("input_food_code");
+    
+    let productReference = document.getElementById("floatingSelectProduct").value;
+    
+    fetch(productReference)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { 
+
+        // fill the food consumption fields
+        inputFoodName.value = data["name"];
+        inputFoodCode.value = data["code"];
+    })
+    .catch( error => console.error(error) );  // error handle
+}
+
+
+
+/**
+ * Inital calls
+ */
+loadMaterials();
 getProductsByName();
