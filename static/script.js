b1a1d273d9fb16e57aff9957d70c0295b332e06d
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-15 11:33:09 +0200

    search food function

--- static/script.js
+++ static/script.js
@@ -10,3 +10,24 @@ function loadMaterials(){
         console.log(data);
     });
 }
+
+function getProductsByName() {
+
+    let item_name = document.getElementById("input_food_item").value;
+    
+    fetch('/api/v1/products')
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { // Here you get the data to modify as you please
+        
+        data.forEach(element => {
+            if (element["name"].includes(item_name)){
+                console.log(element["name"])
+            }
+        });
+
+    })
+    .catch( error => console.error(error) );// If there is any error you will catch them here
+    
+}
+
+getProductsByName();
