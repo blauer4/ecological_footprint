ce869306aca1c5dc626aa369c5031c6dc436a070
TudiFrudii <vittoria.ossanna@studenti.unitn.it>
2022-05-17 16:52:01 +0200

    added swagger documentation for material and garbageActivity

--- app/models/garbageActivity.js
+++ app/models/garbageActivity.js
@@ -1,3 +1,48 @@
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          GarbageActivity:
+ *              type: object
+ *              required:
+ *                  - userId
+ *                  - date
+ *                  - impact
+ *                  - material
+ *                  - amount
+ *              properties:
+ *                  userId:
+ *                      type: string
+ *                      description: automatically assigned ID to the specific activity.
+ *                  date: 
+ *                      type: string
+ *                      format: date
+ *                      description: date of insertion of the activity.
+ *                  impact: 
+ *                      type: integer
+ *                      description: total impact of the activity, This value is calculated as the amount of the garbage disposed multiplied for the impact of the material disposed
+ *                  material:
+ *                      type: object
+ *                      properties:
+ *                          name: 
+ *                              type: string
+ *                              description: the name of the material to dispose.
+ *                          unitImpact: 
+ *                              type: integer
+ *                              description: The value of the impact of a single unit of that material. 
+ *                  amount:
+ *                      type: integer
+ *                      description: number of bags disposed
+ *              example:
+ *                  userId: 34567gtfgyu67890mmkjm
+ *                  date: 2022-05-17T12:46:47.215Z
+ *                  impact: 333
+ *                  material: {Umido, 1}
+ *                  amount: 2
+ *                  
+ */
+
+
 var mongoose = require('mongoose');
 const materialSchema = require('./material.js').materialSchema;
 var Schema = mongoose.Schema;
--- index.js
+++ index.js
@@ -2,17 +2,56 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 const app = require("./app/app.js");
 
-mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then (() => {
-    
-    console.log("Connected to the Database");
-    
-    app.listen(process.env.EXPRESS_PORT, function() {
-        console.log('Server running on port ', process.env.EXPRESS_PORT);
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUI = require("swagger-ui-express");
+
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+
+        const options = {
+            definition: {
+                openapi: "3.0.0",
+                info: {
+                    title: "LogRocket Express API with Swagger",
+                    version: "0.1.0",
+                    description:
+                        "This is a simple CRUD API application made with Express and documented with Swagger",
+                    license: {
+                        name: "MIT",
+                        url: "https://spdx.org/licenses/MIT.html",
+                    },
+                    contact: {
+                        name: "LogRocket",
+                        url: "https://logrocket.com",
+                        email: "info@email.com",
+                    },
+                },
+                servers: [
+                    {
+                        url: "http://localhost:3000",
+                    },
+                ],
+            },
+            apis: ["./app/models/*.js"],
+        };
+
+        const specs = swaggerJsdoc(options);
+        app.use(
+            "/api-docs",
+            swaggerUI.serve,
+            swaggerUI.setup(specs)
+        );
+
+        console.log("Connected to the Database");
+
+        app.listen(process.env.EXPRESS_PORT, function () {
+            console.log('Server running on port ', process.env.EXPRESS_PORT);
+        });
+
+    })
+    .catch(() => {
+        console.error("ERROR: db connection error");
     });
-    
-})
-.catch(()=>{
-    console.error("ERROR: db connection error");
-});
 
