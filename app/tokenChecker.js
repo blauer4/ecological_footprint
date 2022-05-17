c61d155ce9bf4ea1431894399c67c5c94d2850a7
Laurence Bonat <laurence.bonat@studenti.unitn.it>
2022-05-17 16:32:31 +0200

    Redirect user to login on every landing page

--- app/tokenChecker.js
+++ app/tokenChecker.js
@@ -3,7 +3,10 @@ const jwt = require("jsonwebtoken")
 const tokenChecker = function(req, res, next) {
     
     let token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if (!token) res.redirect("login.html").json({success:false, message:'No token provided.'});
+    if (!token) {
+        res.redirect("login.html");
+        return;
+    }
 
     jwt.verify(token, process.env.SUPER_SECRET, function(err, decoded){
         if (err) res.status(403).json({success:false, message:'Token not valid'})
