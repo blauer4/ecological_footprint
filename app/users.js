b33d6dc765c19e89c52412abaeaaecb1d0e40b17
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-18 16:24:11 +0200

    user handle register

--- app/users.js
+++ app/users.js
@@ -2,6 +2,32 @@ const express = require('express');
 const User = require('./models/user').User;
 const router = express.Router();
 
+router.get('', async (req, res) => {
+ 
+    let users = await User.find({});
+    users = users.map( (user) => {
+        return {
+            self: '/api/v1/users/' + user["_id"],
+            username: user["username"]
+        };
+    });
+    res.status(200).json(users);
+});
+
+router.get('/:id', async (req, res) => {
+
+    let user = await User.findById(req.params.id);
+    user = {
+        self: '/api/v1/users/' + user.id,
+        username: user["username"],
+        name: user["name"],
+        surname: user["surname"],
+        email: user["email"]
+    }
+
+    res.status(200).json(user);
+});
+
 router.post('', async (req, res) => {
     let username = req.body.username;
     let name = req.body.name;
