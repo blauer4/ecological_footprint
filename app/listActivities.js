06aafa7217861801e4fce23b766b6a32fde0792f
TudiFrudii <vittoria.ossanna@studenti.unitn.it>
2022-05-19 11:55:13 +0200

    fixing global impact calculation

--- app/listActivities.js
+++ app/listActivities.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const jwt = require("jsonwebtoken")
 const garbageActivity = require('./models/garbageActivity.js');
 const productActivity = require('./models/productActivity.js');
 const transportActivity = require('./models/transportActivity.js');
 
 const router = express.Router();
 
-router.get('',async (req,res)=>{
+router.get('', async (req, res) => {
     let garbage = await garbageActivity.find({});
     let product = await productActivity.find({});
     let transport = await transportActivity.find({});
     // TODO: Select only authenticated
     let resp = [];
-    for(item of product){
+    for (item of product) {
         resp.push({
             self: "/api/v1/activities/products/" + item.id,
             type: "product",
@@ -21,7 +20,7 @@ router.get('',async (req,res)=>{
         });
     }
 
-    for(item of garbage){
+    for (item of garbage) {
         resp.push({
             self: "/api/v1/activities/garbage/" + item.id,
             type: "garbage",
@@ -30,7 +29,7 @@ router.get('',async (req,res)=>{
         });
     }
 
-    for(item of transport){
+    for (item of transport) {
         resp.push({
             self: "/api/v1/activities/transport/" + item.id,
             type: "transport",
@@ -41,40 +40,32 @@ router.get('',async (req,res)=>{
     res.status(200).json(resp);
 });
 
-router.get('/total_impact',async (req,res)=>{
-    let token = req.cookies['token'];
+router.get('/total_impact', async (req, res) => {
+    console.log(req.cookies);
+    let userId = req.cookies['userId'];
     let garbage, product, transport;
-    let search = {};
-    if (token) {
-        jwt.verify(token, process.env.SUPER_SECRET, function(err, decoded){
-            if (err) {  // if the token is invalid redirect to the login page
-                res.redirect("/login.html");
-            }else {
-                req.loggedUser = decoded;
-                console.log("decoded:", decoded);
-                search = {userId: decoded.id};
-            }
-        });
-    } 
-    garbage = await garbageActivity.find(search);
-    product = await productActivity.find(search);
-    transport = await transportActivity.find(search);
+    let search = false;
+    if (userId) {
+        search = { userId: userId };
+    }
+    garbage = await garbageActivity.find(search ? search : {});
+    product = await productActivity.find(search ? search : {});
+    transport = await transportActivity.find(search ? search : {});
     // TODO: Select only authenticated
     let total_impact = 0;
     let resp;
-    for(item of product){
+    for (item of product) {
         total_impact += item.impact;
     }
 
-    for(item of garbage){
+    for (item of garbage) {
         total_impact += item.impact;
     }
 
-    for(item of transport){
+    for (item of transport) {
         total_impact += item.impact;
     }
-    resp = {"total_impact" : total_impact};
-    console.log(resp, garbage, product, transport);
+    resp = { "total_impact": total_impact };
     res.status(200).json(resp);
 });
 
--- static/authentication/index.js
+++ static/authentication/index.js
@@ -6,13 +6,12 @@
     let somma = 0;
     let counter = 0;
     
-    fetch('/api/v1/activities/total_impact')
+    fetch('/api/v1/generalStats/total_impact')
     .then((resp) => resp.json())
     .then(function (data) {
            let h1 = document.getElementById("average_placeholder");
-           h1.innerHTML = data["total_impact"];
+           h1.innerHTML = data.total_impact;
         }).catch(error => console.error(error));
-    console.log(somma/counter);
 }
 
 loadGlobalImpact();
