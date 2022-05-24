80521079f2643ab4e4efc762380c0a397eee4e13
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-24 22:51:42 +0200

    get a friend with a particular id

--- app/friends.js
+++ app/friends.js
@@ -7,14 +7,36 @@ router.get('', async (req, res) => {
     let currentUser = await User.findById(req.loggedUser.id);
     let friends = currentUser.friends;
 
-    console.log(friends)
     friends = friends.map( (friend) => {
         return {
-            self: '/api/v1/users/' + friend["_id"],
+            self: '/api/v2/friends/' + friend["_id"],
             name: friend["name"]
         };
     });
     res.status(200).json(friends);
+});
+
+
+router.get('/:id', async (req, res) => {
+
+    let currentUser = await User.findById(req.loggedUser.id);
+    let friends = currentUser.friends;
+
+    let user = req.params.id;
+    // if the user you are searching is in my friend list return it
+    if (user in friends){
+        user = {
+            self: '/api/v1/users/' + user.id,
+            username: user["username"],
+            name: user["name"],
+            surname: user["surname"],
+            email: user["email"]
+        };
+
+        res.status(200).json(user);
+    }   
+
+    res.status(404).send("Not found");
 });
 
 module.exports = router;
