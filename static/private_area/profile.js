4979f4b21b78c75882410b3add8e14ce2df3aec1
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-25 16:34:28 +0200

    add a friend function

--- static/private_area/profile.js
+++ static/private_area/profile.js
@@ -21,4 +21,53 @@ function getAllFollowing() {
     });
 }
 
+function addFriend(){
+    let friendName = document.getElementById("followNewUserName").value;
+
+    fetch(`/api/v1/users`)
+    .then((resp) => resp.json()) 
+    .then(function(availableUsers) { 
+        
+        let userId;
+        for (let i=0; i<availableUsers.length; i+=1){
+            let user = availableUsers[i];
+
+            if (user.username == friendName){
+                userId = user.self.substring(user.self.lastIndexOf('/') + 1);;
+                break;
+            }
+        }
+
+        if (!userId){
+            document.getElementById("error_add_user").innerHTML = "Unable to find a user with that username.";
+            return;
+        }else{
+            document.getElementById("error_add_user").innerHTML = "";
+        }
+
+
+        fetch('/api/v2/friends', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({userId: userId}),
+        })
+        .then((resp) => {
+            if (resp.status == 200 ){
+                document.getElementById("error_add_user").innerHTML = "Friend added";
+            }else{
+                resp.text().then((msg)=>{
+                    document.getElementById("error_add_user").innerHTML = msg;
+                }).catch(error => console.error(error))
+            }
+            
+
+        })
+        .catch( error => console.error(error) ); // error handle
+
+
+        
+    });
+
+}
+
 getAllFollowing();
