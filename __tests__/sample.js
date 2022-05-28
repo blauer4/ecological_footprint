7b584a8c26da421642de609da0ef94b3e3175ccd
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-05-28 12:33:47 +0200

    sample unit test to familiarize

---
+++ __tests__/sample.js
@@ -0,0 +1,17 @@
+describe("Sample sum unit test", () => {
+
+    test("the sum of 1 + 1 should be equal to 2", () => {
+        let result = 1 + 1;
+
+        expect(result).toEqual(2);
+
+    });
+
+    test("the sum of 10 + 5 should be equal to 15", () => {
+        let result = 10 + 5;
+
+        expect(result).toEqual(15);
+
+    });
+
+});
